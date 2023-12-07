d66ccc9311bf7dcd69a44d203b1888dc1dca57dc
jvitorsm <joaovsm04@gmail.com>
2023-12-07 01:34:10 -0300

    Commit 07122023 - criaçao de nova pasta com a lista

---
+++ Lista_de_palavras.py
@@ -0,0 +1,19 @@
+lista_de_palavras = [
+        "Abacaxi", "Borboleta", "Cachorro", "Diversidade", "Elefante",
+        "Futebol", "Girassol", "Harmonia", "Inovação", "Jardim",
+        "Ketchup", "Laranja", "Melodia", "Notável", "Ondulado",
+        "Pintura", "Quimera", "Radiante", "Sombra", "Travesseiro",
+        "Utopia", "Vinho", "Xadrez", "Zoológico","Verve"
+        "Aventura", "Bambu", "Cachecol", "Diamante", "Elegância",
+        "Fantasia", "Gargalhada", "Hortelã", "Ímpeto", "Jubiloso",
+        "Límpido", "Mágico", "Nostalgia", "Orquídea", "Piquenique",
+        "Quasar", "Rústico", "Sincero", "Tremor", "Uva","Vortex",
+        "Aquarela", "Brilhante", "Céu", "Delicioso", "Estrela",
+        "Frio", "Granizo", "Holograma", "Incrível", "Janela",
+        "Kung Fu", "Luar", "Maravilha", "Nebulosa", "Oceano",
+        "Perfume", "Querubim", "Riso", "Sabedoria", "Templo",
+        "Universo", "Voo", "Wok", "Xícara", "Yacht",
+        "Zumbido", "Afável", "Beleza", "Cachoeira", "Desejo",
+        "Euforia", "Fulgor", "Gratidão", "Harmonia", "Inspiração",
+        "Júbilo", "Luminoso", "Místico", "Oásis", "Pétala",
+        "Querência", "Ressurreição", "Solene", "Transcendental", "Utopia"]
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from Lista_de_palavras import lista_de_palavras
 
 def vitoria():
     print("PARABÉNS!!!")
@@ -11,25 +12,7 @@ def jogar():
     print("+++Bem vindo ao jogo da Forca!+++")
     print("+++++++++++++++++++++++++++++++++")
     
-    lista_de_palavras = [
-        "Abacaxi", "Borboleta", "Cachorro", "Diversidade", "Elefante",
-        "Futebol", "Girassol", "Harmonia", "Inovação", "Jardim",
-        "Ketchup", "Laranja", "Melodia", "Notável", "Ondulado",
-        "Pintura", "Quimera", "Radiante", "Sombra", "Travesseiro",
-        "Utopia", "Vinho", "Xadrez", "Zoológico","Verve"
-        "Aventura", "Bambu", "Cachecol", "Diamante", "Elegância",
-        "Fantasia", "Gargalhada", "Hortelã", "Ímpeto", "Jubiloso",
-        "Límpido", "Mágico", "Nostalgia", "Orquídea", "Piquenique",
-        "Quasar", "Rústico", "Sincero", "Tremor", "Uva","Vortex",
-        "Aquarela", "Brilhante", "Céu", "Delicioso", "Estrela",
-        "Frio", "Granizo", "Holograma", "Incrível", "Janela",
-        "Kung Fu", "Luar", "Maravilha", "Nebulosa", "Oceano",
-        "Perfume", "Querubim", "Riso", "Sabedoria", "Templo",
-        "Universo", "Voo", "Wok", "Xícara", "Yacht",
-        "Zumbido", "Afável", "Beleza", "Cachoeira", "Desejo",
-        "Euforia", "Fulgor", "Gratidão", "Harmonia", "Inspiração",
-        "Júbilo", "Luminoso", "Místico", "Oásis", "Pétala",
-        "Querência", "Ressurreição", "Solene", "Transcendental", "Utopia"]
+    
 
     palavra = random.choice(lista_de_palavras).lower()
     letras_corretas = '_'*len(palavra)
